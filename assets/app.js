0cc4436fe70261e1181a3ddeca1e527d2b382d60
CECK <az.ah.qwerty@gmail.com>
2022-10-12 23:33:14 +0500

    movement

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,54 @@
 let character = document.querySelector(".character")
 
 let jump = 300
-document.body.addEventListener('keydown', (e)=>{
-    if(e.key ===" "){
-        
-            
-        
-        character.style.transform = `translateY(-${jump}px)`
-        jump+=100
-         
+let move = 1
 
-       setTimeout(() => {
-        character.style.transform = `translateY(-${0}px)`
-        
-       }, 600);
+document.body.addEventListener('keydown', (e)=>{
+
+    console.log(e.key); 
+if(e.key==="ArrowLeft"){
+    move+=50       
+    character.style.translate= `-${move}px`
+    console.log("AD", move);
+  }
+  if(e.key==="ArrowRight"){
+    move-=50       
+    character.style.translate= `${move}px`
+    console.log("AD", move);
+  }
+
+    if(e.key ===" "){
+        character.style.transform.translateY = `-${jump}px`
     }
-    else(
-        character.style.transform = `translateY(${0}px)`
-    )
+   
 })
+
+
+document.body.addEventListener('keypress', (e)=>{
+    if(e.key==="ArrowLeft"){
+        move+=50       
+        character.style.translate= `${move}px`
+        console.log("AD", move);
+      }
+      if(e.key==="ArrowRight"){
+        move-=50      
+        character.style.translate= `${move}px`
+      }
+    if(e.key===' '){
+        jump+=200
+
+        console.log(jump);      
+        character.style.transform = `translateY(-${jump}px)`
+    }
+})
+
+
+document.body.addEventListener('keyup', (e)=>{
+    if(e.key===' '){
+        setTimeout(() => {
+            character.style.transform = `translateY(-${0}px)` 
+            jump=300
+           }, 600);
+    }
+})
+
