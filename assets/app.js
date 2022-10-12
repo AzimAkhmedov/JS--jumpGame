fe4cd8099e8dd98aea95d5dc1ea46a80b5ee0ecc
CECK <az.ah.qwerty@gmail.com>
2022-10-12 21:24:31 +0500

    jump

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,21 @@
-document.body.addEventListener('keydown', (e)=>{
+let character = document.querySelector(".character")
 
+let jump = 300
+document.body.addEventListener('keydown', (e)=>{
+    if(e.key ===" "){
+        
+            
+        
+        character.style.transform = `translateY(-${jump}px)`
+        jump+=100
+         
+
+       setTimeout(() => {
+        character.style.transform = `translateY(-${0}px)`
+        
+       }, 600);
+    }
+    else(
+        character.style.transform = `translateY(${0}px)`
+    )
 })
